daa0fc98f87e2e8e79ef4e94d60e6b700a57cb9b
Den <technokratos@mail.ru>
2016-05-31 17:04:32 +0300

    SocketContainer

---
+++ src/org/sciTest/SocketContainer.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Created by den on 31/05/2016.
+ */
+public class SocketContainer {
+    public static Socket socket = null;
+    public static boolean init() {
+        try {
+            socket = new Socket("localhost", 3128);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+}
